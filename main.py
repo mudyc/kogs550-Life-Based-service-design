998ba0272a3d59ef3438d8f155611d75f47f8e7a
Matti Katila <matti.katila@gmail.com>
2017-04-10 21:46:00 +0300

    Update posting

--- main.py
+++ main.py
@@ -63,7 +63,8 @@ def diary():
 def save_report():
     with open('DATA.txt', 'a') as f:
         f.write('{:%Y-%m-%d %H:%M:%S} '.format(datetime.datetime.now()))
-        client_ip = request.environ.get('REMOTE_ADDR')
+        client_ip = request.environ.get('HTTP_X_REAL_IP')
+        client_ip += request.environ.get('HTTP_X_FORWARDER_FOR')
         f.write(client_ip)
         f.write(' ')
         f.write(str(request.forms.items()))
